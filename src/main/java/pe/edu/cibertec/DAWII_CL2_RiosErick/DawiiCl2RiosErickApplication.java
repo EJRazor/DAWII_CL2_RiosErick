f53e09c1f56e19d940f8c88ceb55244c63033361
EJRazor <erickju01@gmail.com>
2023-05-29 23:17:17 -0400

    Proyecto y estructura creada

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_RiosErick/DawiiCl2RiosErickApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_RiosErick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2RiosErickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2RiosErickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL2_RiosErick/DawiiCl2RiosErickApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_RiosErick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2RiosErickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
